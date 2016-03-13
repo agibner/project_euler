a644ea6ebb4a12fb0aa63a2c7af4757c9d27cdfa
agibner <agibner@utexas.edu>
2016-03-12 19:31:30 -0600

    sumprimes problem

---
+++ sumprimes.py
@@ -0,0 +1,18 @@
+
+from math import sqrt
+
+def prime(number):
+	for x in range(2, int(sqrt(number)+1)):
+		if number % x == 0:	
+			return False
+	return True
+
+num = 2;
+sum = 0;
+below = input("Sum of primes below: ")
+
+while num<below:
+	if prime(num):	
+		sum = sum+num
+	num = num+1
+print sum
